7fc78d4149c9002b8f797ed97987527125c5ff40
Jessica Schmitz <schmitz.jessical@gmail.com>
2018-04-27 15:04:50 -0700

    add UI search feature

--- src/api.js
+++ src/api.js
@@ -1,12 +1,14 @@
 import $ from 'jquery';
-import './main.js';
+import {getFormVal} from './main.js';
 
 
 class Api {
   makeApiCall(){
+    let queryy = getFormVal();
+    console.log('yolo',getFormVal())
     return new Promise(function(resolve, reject){
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=smith&location=or-portland&skip=0&limit=10&user_key=${process.env.exports.apiKey}`
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${queryy}&location=or-portland&skip=0&limit=80&user_key=${process.env.exports.apiKey}`
       request.onload = function() {
         if(this.status === 200) {
           resolve(request.response);
--- src/main.js
+++ src/main.js
@@ -3,26 +3,41 @@ import { Api } from './api.js';
 import $ from 'jquery';
 import './styles.css';
 
+function getFormVal(){
+  return $('#symptomSearch').val();
+}
+
 $(document).ready(function(){
   console.log("yellow");
   $('#symptomForm').submit(function(event){
     event.preventDefault();
-    let api = new Api();
-    let query = $('#symptomForm').val();
-    let promise = api.makeApiCall();
-    let docArr = [];
 
+    let api = new Api();
+    let promise = api.makeApiCall();
     promise.then(function(response){
+      console.log('yolo');
       let body = JSON.parse(response);
+      console.log('here', body);
       let arrayLimit = body.meta.limit;
       for(let i = 0; i < arrayLimit; i++) {
         let first_name = body.data[i].profile.first_name;
         let last_name = body.data[i].profile.last_name;
         let address = body.data[i].practices[0].visit_address.street + ", " + body.data[i].practices[0].visit_address.city + ", " + body.data[i].practices[0].visit_address.state + ", " + body.data[i].practices[0].visit_address.zip;
-        let website = body.data[i].practices[0].website;
+        let website;
+        if(body.data[i].practices[0].website === undefined){
+          website = "Does not have";
+        } else{
+          website = body.data[i].practices[0].website;
+        }
         let phone = body.data[i].practices[0].phones[0].number;
-        let newPatients = "Accepts new patients: " + body.data[i].accepts_new_patients;
-        console.log("yeeet", first_name, last_name, address, website, phone, newPatients);
+        let newPatients = "Accepts new patients: " + body.data[i].practices[0].accepts_new_patients;
+
+        $("#output").append(`<p>Dr: ${first_name} ${last_name}<br>
+        Address: ${address}<br>
+        Website: ${website}<br>
+        Phone Number: ${phone}<br>
+        ${newPatients}
+        </p>`);
       }
 
 
@@ -30,3 +45,5 @@ $(document).ready(function(){
     });
   });
 });
+
+export {getFormVal}
