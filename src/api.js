20f76a7408d92e29bf5db249a9e0a5aaa4628e39
Jessica Schmitz <schmitz.jessical@gmail.com>
2018-04-27 13:26:32 -0700

    get working response for one doctor

--- src/api.js
+++ src/api.js
@@ -6,7 +6,7 @@ class Api {
   makeApiCall(){
     return new Promise(function(resolve, reject){
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=eczema&location=or-portland&skip=0&limit=1&user_key=${process.env.exports.apiKey}`
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=eczema&location=or-portland&skip=0&limit=10&user_key=${process.env.exports.apiKey}`
       request.onload = function() {
         if(this.status === 200) {
           resolve(request.response);
@@ -18,9 +18,5 @@ class Api {
       request.send();
     });
   }
-  // showResult(response){
-  //   $("#output").empty();
-  //   for(let i = 0; i < )
-  // }
 }
 export {Api};
--- src/main.js
+++ src/main.js
@@ -10,14 +10,21 @@ $(document).ready(function(){
     let api = new Api();
     let query = $('#symptomForm').val();
     let promise = api.makeApiCall();
-
-
+    let docArr = [];
 
     promise.then(function(response){
-      let body = JSON.parse(response)
+      let body = JSON.parse(response);
+      function getResults(response) {
+        let first_name = body.data[0].profile.first_name;
+        let last_name = body.data[0].profile.last_name;
+        let address = body.data[0].visit_address.street + ", " + body.data[0].visit_address.city + ", " + body.data[0].visit_address.state + ", " + body.data[0].visit_address.zip;
+        let website = body.data[0].practices[0].website;
+        let phone = body.data[0].practices[0].phones[0].number;
+        let newPatients = "Accepts new patients: " + body.data[0].accepts_new_patients;
+      }
 
-      console.log("yeeet", response);
-      $('#output').html(`<li>${body.data[0].practices[0].phones.number}</li>`);
+      console.log("yeeet", body);
+
     });
   });
 });
