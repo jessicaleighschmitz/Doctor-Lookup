0ef54c764ca25240afb601e7407ffeb80848d368
Jessica Schmitz <schmitz.jessical@gmail.com>
2018-04-27 09:33:09 -0700

    add basic html structure

--- src/api.js
+++ src/api.js
@@ -4,6 +4,18 @@ import './main.js';
 
 class Api {
   makeApiCall(){
-
+    return new Promise(function(resolve, reject){
+      let request = new XMLHttpRequest();
+      let url = ``
+      request.onload = function() {
+        if(this.status === 200) {
+          resolve(request.response);
+        } else {
+          rejesct(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,4 +1,21 @@
 // empty
-import {  } from './scripts';
+import { Api } from './api.js';
 import $ from 'jquery';
 import './styles.css';
+
+$(document).ready(function(){
+  $('div').submit(function(event){
+    event.preventDefault();
+
+promise.then(function(response){
+  let body = JSON.parse(repsonse)
+})
+
+
+
+
+
+
+
+  })
+})
